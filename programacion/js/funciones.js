c54b371f82b78bbcdcbade3240ddf2f144cbab97
FacundoFigun <129554455+FacundoFigun@users.noreply.github.com>
2023-04-19 14:23:56 -0300

    Add files via upload

---
+++ programacion/js/funciones.js
@@ -0,0 +1,12 @@
+const aux = document.getElementById("cajatexto");
+
+function leerTexto(){
+    var apellido = document.getElementById("apellido").value;
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value;
+
+    console.log(apellido);
+    console.log(nombre);
+    console.log(edad);
+}
+
