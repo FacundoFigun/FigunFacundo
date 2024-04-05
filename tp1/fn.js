9e9fc69b99b5298b144ad89cdf428635e9f37be2
FacundoFigun <129554455+FacundoFigun@users.noreply.github.com>
2024-04-05 20:50:47 -0300

    Add files via upload

---
+++ tp1/fn.js
@@ -0,0 +1,20 @@
+var frutas = ['Pera', 'Manzana', 'Frutilla','Mandarina','Tomate'];
+
+//console.log (frutas[1]);
+
+
+ /*  for(i = 0; i<= frutas.length; i++){
+       console.log(frutas[i])
+    } */
+
+  /* frutas.forEach(fruta=>{
+    console.log(fruta)
+   }) */
+
+   for(const fruta of frutas){
+    if (fruta == 'Manzana'){
+        console.log(fruta)
+    }
+    
+   };
+
